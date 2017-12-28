1e3dae49c923d9641007043d587d973871fd655a
lesiak <lesiak@outlook.com>
2017-12-28 08:26:42 +0100

    Add project files.

---
+++ FizzBuzzFunctional/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FizzBuzzFunctional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
